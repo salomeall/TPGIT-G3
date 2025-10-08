dac32ecbc1ebc324449e55ed8673d53ecf00e2c8
Bayshunpu <nolwen.berton@outlook.fr>
2025-10-08 17:26:23 +0200

    Add Adresse

---
+++ NintendoProject/src/main/java/nintendo/model/Adresse.java
@@ -0,0 +1,39 @@
+package nintendo.model;
+
+public class Adresse {
+	
+	private Integer numero;
+	private String rue;
+	private String ville;
+	public Adresse(Integer numero, String rue, String ville) {
+		super();
+		this.numero = numero;
+		this.rue = rue;
+		this.ville = ville;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	@Override
+	public String toString() {
+		return "Adresse [numero=" + numero + ", rue=" + rue + ", ville=" + ville + "]";
+	}
+	
+	
+
+}
