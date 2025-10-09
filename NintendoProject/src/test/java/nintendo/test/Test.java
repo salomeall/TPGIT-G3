f4ef15268faa14a367934df45c205c52a8ce504c
salom <salome.allali.kg@gmail.com>
2025-10-09 09:35:12 +0200

    ajout console

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,9 +1,23 @@
 package nintendo.test;
 
+import nintendo.model.Console;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Console c1 = new Console("Nintendo Switch");
+		Console c2 = new Console("Nintendo DS");
+		Console c3 = new Console("Nintendo Wii");
+		Console c4 = new Console("Nintendo Gameboy");
+		Console c5 = new Console("Nintendo GameCube");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		
 
 	}
 
